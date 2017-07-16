42df10bf7c93e0ce09831262af0993500779a1dd
janesjo <jonathan.janesjo@outlook.com>
2017-07-17 01:55:48 +0200

    Init

---
+++ MacTestApp/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace MacTestApp
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
